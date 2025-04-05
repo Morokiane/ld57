50cb836dd2589c795d050e6200768821ba680e05
BSS <morokiane@gmail.com>
2025-04-05 12:21:17 -0600

    05.04.25

--- Assets/Scripts/Grave.cs
+++ Assets/Scripts/Grave.cs
@@ -1,26 +1,38 @@
 using Unity.Mathematics;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class Grave : MonoBehaviour {
     [SerializeField] private Sprite[] gravestone;
-    [SerializeField] private GraveDescription description;
+    [SerializeField] private GraveDescription[] description;
     [SerializeField] private GameObject uiPanel;
     [SerializeField] private TMPro.TextMeshProUGUI[] descriptionText;
     [SerializeField] private GameObject soulObject;
 
     private bool hasSoul = true;
+    private SpriteRenderer spriteRender;
+    private GraveDescription currentDescription;
     
     // Showing the description is lagging so preloading to avoid the lag spike on the first time
     private void Start() {
         uiPanel.SetActive(true);
         Canvas.ForceUpdateCanvases();
         uiPanel.SetActive(false);
+        spriteRender = GetComponent<SpriteRenderer>();
+
+        spriteRender.sprite = gravestone[Random.Range(0, gravestone.Length)];
     }
 
     private void OnTriggerEnter2D(Collider2D _other) {
         if (_other.CompareTag("Player")) {
+            ChooseRandomDescription();
             ShowDescription();
             Player.instance.canDig = true;
+        }
+    }
+    private void ChooseRandomDescription() {
+        if (description.Length > 0) {
+            currentDescription = description[Random.Range(0, description.Length)];
         }
     }
 
@@ -34,8 +46,8 @@ public class Grave : MonoBehaviour {
     private void ShowDescription() {
         uiPanel.SetActive(true);
         
-        for (int i = 0; i < descriptionText.Length && i < description.descriptionLines.Length; i++) {
-            descriptionText[i].text = description.descriptionLines[i];
+        for (int i = 0; i < descriptionText.Length && i < currentDescription.descriptionLines.Length; i++) {
+            descriptionText[i].text = currentDescription.descriptionLines[i];
         }
     }
     
@@ -50,7 +62,7 @@ public class Grave : MonoBehaviour {
             Soul soul = soulGO.GetComponent<Soul>();
 
             if (soul != null) {
-                soul.Initilize(description);
+                soul.Initilize(currentDescription);
             }
         }
     }
