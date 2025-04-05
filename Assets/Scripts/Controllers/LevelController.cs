2e7090183ce10e76db48d56d3c2d0ff52b3ea438
BSS <morokiane@gmail.com>
2025-04-05 16:37:14 -0600

    starting over

--- Assets/Scripts/Controllers/LevelController.cs
+++
@@ -1,7 +0,0 @@
-using UnityEngine;
-
-namespace Controllers {
-    public class LevelController : MonoBehaviour {
-        public static uint soulWeight;
-    }
-}
--- Assets/Scripts/GraveDescription.cs
+++
@@ -1,7 +0,0 @@
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "New Description", menuName = "Descriptions/Grave", order = 0)]
-public class GraveDescription : ScriptableObject {
-    [TextArea(3, 5)] public string[] descriptionLines = new string[3];
-    public int weight;
-}
