a48b7c135bf66d49fe104fc46a6e079b80647809
BSS <morokiane@gmail.com>
2025-04-06 12:53:51 -0600

    06.05.25

--- Assets/Scripts/Utils/DontDestroy.cs
+++ Assets/Scripts/Utils/DontDestroy.cs
@@ -2,7 +2,15 @@ using UnityEngine;
 
 namespace Utils {
     public class DontDestroy : MonoBehaviour {
+        public static DontDestroy instance;
+        
         private void Awake() {
+            if (instance == null) {
+                instance = this;
+            } else {
+                Destroy(gameObject);
+            }
+            
             DontDestroyOnLoad(gameObject);
         }
     }
